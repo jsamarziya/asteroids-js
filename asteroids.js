d1ea34d0e22d31bce65d9d5a2f9577729a82e425
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2016-07-20 01:50:10 -0700

    added input manager

--- asteroids.js
+++ asteroids.js
@@ -4,18 +4,24 @@ function Asteroids(window, canvas) {
     this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
     this.lastUpdate = 0;
-    this.initCanvas();
+    this.inputManager = new InputManager(window);
+    this.init();
 }
 
-Asteroids.prototype.initCanvas = function () {
+Asteroids.prototype.init = function () {
     this.ctx.strokeStyle = "white";
     this.ctx.fillStyle = "white";
 };
 
+Asteroids.prototype.run = function () {
+    this.update();
+};
+
 Asteroids.prototype.update = function (timestamp) {
     this.window.requestAnimationFrame(this.update.bind(this));
-    var delta = timestamp - this.lastUpdate;
+    var dt = timestamp - this.lastUpdate;
     this.lastUpdate = timestamp;
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.fillText(delta, 100, 100);
+    this.ctx.fillText("dt: " + dt, 10, 100);
+    this.ctx.fillText("keysDown: " + this.inputManager.getKeysDown(), 10, 200);
 };
---
+++ input.js
@@ -0,0 +1,31 @@
+function InputManager(window) {
+    this.keysDown = {};
+
+    window.document.addEventListener('keydown', this.keyDown.bind(this));
+
+    window.document.addEventListener('keyup', this.keyUp.bind(this));
+
+    window.addEventListener('blur', this.clearKeysDown.bind(this));
+}
+
+InputManager.prototype.clearKeysDown = function () {
+    this.keysDown = {};
+};
+
+InputManager.prototype.keyDown = function (event) {
+    this.keysDown[event.key] = true;
+};
+
+InputManager.prototype.keyUp = function (event) {
+    delete this.keysDown[event.key];
+};
+
+InputManager.prototype.isKeyDown = function (key) {
+    return key in this.keysDown;
+};
+
+InputManager.prototype.getKeysDown = function () {
+    return Object.keys(this.keysDown).reduce((p, c) => {
+        return p + c;
+    }, "");
+};
