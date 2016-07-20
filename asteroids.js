0dbeb40703a2f3a9a2313fe4fff02b4574af94d5
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2016-07-20 13:24:42 -0700

    added delta-time limit

--- asteroids.js
+++ asteroids.js
@@ -1,4 +1,5 @@
-// The Asteroids game.
+const MAX_DELTA_TIME = 160;
+
 function Asteroids(window, canvas) {
     this.window = window;
     this.canvas = canvas;
@@ -19,8 +20,14 @@ Asteroids.prototype.run = function () {
 
 Asteroids.prototype.update = function (timestamp) {
     this.window.requestAnimationFrame(this.update.bind(this));
-    var dt = timestamp - this.lastUpdate;
+    const dt = timestamp - this.lastUpdate;
     this.lastUpdate = timestamp;
+    if (dt <= MAX_DELTA_TIME) {
+        this.tick(dt);
+    }
+};
+
+Asteroids.prototype.tick = function (dt) {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.fillText("dt: " + dt, 10, 100);
     this.ctx.fillText("keysDown: " + this.inputManager.getKeysDown(), 10, 200);
