904c4175ea1ceeb320e3350826da02fbb36222b0
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2016-07-21 12:51:51 -0700

    redesigned key input handling

--- input.js
+++ input.js
@@ -1,25 +1,42 @@
-function InputManager(window) {
-    this.keysDown = {};
-
-    window.document.addEventListener('keydown', this.keyDown.bind(this));
-
-    window.document.addEventListener('keyup', this.keyUp.bind(this));
-
+function InputManager() {
+    this.keyMap = this.createKeyMap();
+    document.addEventListener('keydown', this.keyDown.bind(this));
+    document.addEventListener('keyup', this.keyUp.bind(this));
     window.addEventListener('blur', this.clearKeysDown.bind(this));
 }
 
-InputManager.prototype.clearKeysDown = function () {
-    this.keysDown = {};
+InputManager.prototype.createKeyMap = function () {
+    return {
+        fullScreen: this.createRequestFullScreenModeKey()
+    };
+};
+
+InputManager.prototype.createRequestFullScreenModeKey = function () {
+    const key = new Key("f");
+    key.addKeyDownListener(function () {
+        asteroids.requestFullScreen();
+    });
+    return key;
 };
 
 InputManager.prototype.keyDown = function (event) {
-    this.keysDown[event.key] = true;
+    Object.keys(this.keyMap).forEach(key => {
+        this.keyMap[key].matches(event) && this.keyMap[key].fireKeyDown();
+    });
     this.preventDefaultEventActionIfRequired(event);
 };
 
 InputManager.prototype.keyUp = function (event) {
-    delete this.keysDown[event.key];
+    Object.keys(this.keyMap).forEach(key => {
+        this.keyMap[key].matches(event) && this.keyMap[key].fireKeyUp();
+    });
     this.preventDefaultEventActionIfRequired(event);
+};
+
+InputManager.prototype.clearKeysDown = function () {
+    Object.keys(this.keyMap).forEach(key => {
+        this.keyMap[key].fireKeyUp();
+    });
 };
 
 InputManager.prototype.preventDefaultEventActionIfRequired = function (event) {
@@ -29,15 +46,49 @@ InputManager.prototype.preventDefaultEventActionIfRequired = function (event) {
 };
 
 InputManager.prototype.shouldPreventDefaultEventAction = function (event) {
-    return event instanceof KeyboardEvent && event.key.startsWith("Arrow");
-};
-
-InputManager.prototype.isKeyDown = function (key) {
-    return key in this.keysDown;
+    return event.key.startsWith("Arrow");
 };
 
 InputManager.prototype.getKeysDown = function () {
-    return Object.keys(this.keysDown).reduce((p, c) => {
-        return p + c;
-    }, "");
+    // return Object.keys(this.keysDown).reduce((p, c) => {
+    //     return p + c;
+    // }, "");
+};
+
+function Key(key, alt, ctrl, meta, shift) {
+    this.key = key;
+    this.alt = !!alt;
+    this.ctrl = !!ctrl;
+    this.meta = !!meta;
+    this.shift = !!shift;
+    this.keyDownListeners = [];
+    this.keyUpListeners = [];
+}
+
+Key.prototype.addKeyDownListener = function (listener) {
+    this.keyDownListeners.push(listener);
+};
+
+Key.prototype.addKeyUpListener = function (listener) {
+    this.keyUpListeners.push(listener);
+};
+
+Key.prototype.matches = function (event) {
+    return this.key == event.key
+        && this.alt == event.altKey
+        && this.ctrl == event.ctrlKey
+        && this.meta == event.metaKey
+        && this.shift == event.shiftKey;
+};
+
+Key.prototype.fireKeyDown = function () {
+    this.keyDownListeners.forEach(listener => {
+        listener();
+    });
+};
+
+Key.prototype.fireKeyUp = function () {
+    this.keyUpListeners.forEach(listener => {
+        listener();
+    });
 };
