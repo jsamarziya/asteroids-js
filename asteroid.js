5f6dcf01e345b830f4c6da3e54ca59d3afb0decc
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2016-07-25 16:36:09 -0700

    tweaked asteroid path algorithm to try to reduce pointiness

--- asteroid.js
+++ asteroid.js
@@ -9,14 +9,20 @@ class Asteroid extends Sprite {
     static createPath(size) {
         let points = [];
         let rotation = 0;
+        let radius = size;
+        let setRadius = true;
         while (true) {
-            let angle = Math.random() * .3 + .2;
+            let angle = Math.random() * .2 + .2;
             rotation += angle;
+            if (setRadius) {
+                radius = (1 - Math.random() * .4) * size;
+            }
+            setRadius = !setRadius;
+            points.push({angle: angle, radius: radius});
             if (rotation > FULL_CIRCLE) {
+                points.push({angle: 0, radius: radius});
                 break;
             }
-            let radius = (1.5 - Math.random() * .5) * size;
-            points.push({angle: angle, radius: radius});
         }
         return points;
     }
@@ -30,10 +36,15 @@ class Asteroid extends Sprite {
         this.path.forEach(point => {
             ctx.rotate(point.angle);
             ctx.lineTo(0, Math.floor(point.radius * scale));
+            // ctx.lineTo(0, 0);
+            // ctx.lineTo(0, Math.floor(point.radius * scale));
         });
         ctx.closePath();
         ctx.fill();
         ctx.stroke();
+        // ctx.beginPath();
+        // ctx.arc(0, 0, 100, 0, 2 * Math.PI);
+        // ctx.stroke();
     }
 }
 
