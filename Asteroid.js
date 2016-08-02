c5208e90376a57baeed07cf99d550bf84e385b53
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2016-08-02 16:42:35 -0700

    added JSDoc

--- Asteroid.js
+++ Asteroid.js
@@ -1,16 +1,40 @@
 "use strict";
 
+/**
+ * The number of radii created by {@link Asteroid#createRadii}.
+ * @type {number}
+ */
 const ASTEROID_RADII = 11;
+/**
+ * The amount (in radians) that the context is rotated between each segment drawn.
+ * @type {number}
+ */
 const ASTEROID_SEGMENT_ROTATION = FULL_CIRCLE / ASTEROID_RADII / 2;
+/**
+ * The size of a large asteroid.
+ * @type {number}
+ */
 const ASTEROID_SIZE_LARGE = 230;
 
+/**
+ * The asteroid sprite.
+ */
 class Asteroid extends Sprite {
+    /**
+     * Constructs a new Asteroid.
+     * @param {Game} game the Game to which the sprite will belong
+     * @param {number} size the size (radius) of the asteroid
+     */
     constructor(game, size) {
         super(game);
         this.radius = size;
         this.radii = this.createRadii();
     }
 
+    /**
+     * Creates the radii of the segments of this asteroid.
+     * @returns {Array}
+     */
     createRadii() {
         const radii = [];
         for (let i = 0; i < ASTEROID_RADII; i++) {
@@ -20,6 +44,10 @@ class Asteroid extends Sprite {
         return radii;
     }
 
+    /**
+     * @inheritDoc
+     * @override
+     */
     drawSprite(ctx) {
         ctx.beginPath();
         this.radii.forEach(radius => {
