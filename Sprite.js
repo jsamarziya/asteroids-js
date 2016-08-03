4d8bf654c294beb3f55f124a00a59e7dfd056568
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2016-08-02 17:42:36 -0700

    added JSDoc

--- Sprite.js
+++ Sprite.js
@@ -1,7 +1,19 @@
 "use strict";
 
+/**
+ * The number of radians in a full circle.
+ * @type {number}
+ */
 const FULL_CIRCLE = 2 * Math.PI;
+/**
+ * The factor used to calculate the distance traveled by a sprite in one millisecond.
+ * @type {number}
+ */
 const SPRITE_VELOCITY_FACTOR = 1 / 1000;
+/**
+ * The number of radians rotated in one millisecond by a sprite rotating at one rotation per minute.
+ * @type {number}
+ */
 const ROTATION_PER_MILLISECOND = FULL_CIRCLE / 60 / 1000;
 
 /**
@@ -24,6 +36,10 @@ class Sprite {
         this.radius = 0;
     }
 
+    /**
+     * Updates the state of this sprite.
+     * @param {number} dt the time delta
+     */
     update(dt) {
         this.rotation += this.rpm * dt * ROTATION_PER_MILLISECOND;
         if (this.rotation > FULL_CIRCLE) {
@@ -45,22 +61,41 @@ class Sprite {
         }
     }
 
+    /**
+     * Returns the number of rotations per minute that this sprite is rotating.
+     * @returns {number} the rotation of this sprite, in rotations per minute
+     */
     get rpm() {
         return this._rpm;
     }
 
+    /**
+     * Sets the number of rotations per minute that this sprite is rotating.
+     * @param {number} rpm the rotation of this sprite, in rotations per minute
+     */
     set rpm(rpm) {
         this._rpm = rpm;
     }
 
+    /**
+     * Returns the x-coordinate of the location of this sprite, scaled to the game canvas
+     * @returns {number} the scaled x-coordinate
+     */
     get scaledX() {
         return this.game.getScaledWidth(this.x);
     }
 
+    /**
+     * Returns the y-coordinate of the location of this sprite, scaled to the game canvas
+     * @returns {number} the scaled y-coordinate
+     */
     get scaledY() {
         return this.game.getScaledHeight(this.y);
     }
 
+    /**
+     * Draws this sprite on the game canvas.
+     */
     draw() {
         const ctx = this.game.gameContext;
         ctx.save();
@@ -92,6 +127,10 @@ class Sprite {
         ctx.restore();
     }
 
+    /**
+     * Draws this sprite.
+     * @param ctx the context to draw to
+     */
     drawInternal(ctx) {
         this.drawSprite(ctx);
         if (this.game.drawDebug) {
