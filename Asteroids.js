283ca22685a509838978e938dc86b0939211215c
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2016-10-27 09:56:31 -0700

    redesigned the game lifecycle

--- Asteroids.js
+++ Asteroids.js
@@ -1,6 +1,12 @@
 "use strict";
 
 /**
+ * The delay (in milliseconds) before a ship or a set of asteroids is added to the play field.
+ * @type {number}
+ */
+const TRANSITION_DELAY = 2000;
+
+/**
  * The Asteroids game.
  */
 class Asteroids extends Game {
@@ -13,9 +19,7 @@ class Asteroids extends Game {
     constructor(container, gameCanvas, debugCanvas) {
         super(container, gameCanvas, debugCanvas);
         this.createStars();
-        this.createShip();
-        this.asteroidCount = 6;
-        this.scheduler.schedule(this.createAsteroids.bind(this), 1000);
+        this.scheduleCreateAsteroids();
     }
 
     /**
@@ -29,14 +33,45 @@ class Asteroids extends Game {
     }
 
     /**
+     * Returns the number of asteroids created by createAsteroids() at the start of a level.
+     * @return {number} the number of asteroids
+     */
+    get asteroidCount() {
+        const level = this.player ? this.player.level : 1;
+        return Math.min((level + 1) * 2, 14);
+    }
+
+    /**
+     * Starts a new game.
+     */
+    startNewGame() {
+        if (this.player && this.player.lives) {
+            return;
+        }
+        this.player = new Player();
+        // TODO we should maybe keep the existing array and just remove all of its elements?
+        this.sprites = [];
+        this.createShip();
+        this.scheduleCreateAsteroids();
+    }
+
+    /**
      * Creates the player's ship.
      */
     createShip() {
+        // TODO check for clear area before creating ship
         const ship = new Ship(this);
         ship.x = REFERENCE_WIDTH / 2;
         ship.y = REFERENCE_HEIGHT / 2;
         this.ship = ship;
         this.addSprite(ship);
+    }
+
+    /**
+     * Schedules a createAsteroids() call.
+     */
+    scheduleCreateAsteroids() {
+        this.scheduler.schedule(this.createAsteroids.bind(this), TRANSITION_DELAY);
     }
 
     /**
@@ -55,6 +90,7 @@ class Asteroids extends Game {
      * @param {number} y the y-coordinate of the asteroid's position
      */
     createAsteroid(type, x, y) {
+        // TODO check for proximity to ship before placing? or caveat player?
         const asteroid = new Asteroid(this, type);
         asteroid.x = x;
         asteroid.y = y;
@@ -92,11 +128,17 @@ class Asteroids extends Game {
      * @inheritDoc
      */
     drawDebugLayerExtensions() {
-        this.debugContext.fillText("pos: (" + Math.floor(this.ship.scaledX) + ", " + Math.floor(this.ship.scaledY) + ")", 0, 0, 100);
+        const ship = this.ship;
+        const x = ship ? Math.floor(ship.scaledX) : '-';
+        const y = ship ? Math.floor(ship.scaledY) : '-';
+        const dx = ship ? Math.round(this.getScaledWidth(ship.dx)) : '-';
+        const dy = ship ? Math.round(this.getScaledHeight(ship.dy)) : '-';
+        const heading = ship ? Math.floor(ship.rotation * 180 / Math.PI) + "\xB0" : '-';
+        this.debugContext.fillText("pos: (" + x + ", " + y + ")", 0, 0, 100);
         this.debugContext.translate(100, 0);
-        this.debugContext.fillText("v: (" + Math.round(this.getScaledWidth(this.ship.dx)) + ", " + Math.round(this.getScaledHeight(this.ship.dy)) + ")", 0, 0, 100);
+        this.debugContext.fillText("v: (" + dx + ", " + dy + ")", 0, 0, 100);
         this.debugContext.translate(100, 0);
-        this.debugContext.fillText("\u03b8: " + Math.floor(this.ship.rotation * 180 / Math.PI) + "\xB0", 0, 0, 100);
+        this.debugContext.fillText("\u03b8: " + heading, 0, 0, 100);
     }
 
     /**
@@ -115,7 +157,7 @@ class Asteroids extends Game {
      */
     asteroidDestroyed() {
         if (this.getSpriteCount(Asteroid) == 0) {
-            this.scheduler.schedule(this.createAsteroids.bind(this), 2000);
+            this.scheduleCreateAsteroids();
         }
     }
 
@@ -124,7 +166,11 @@ class Asteroids extends Game {
      * @inheritDoc
      */
     playerDestroyed() {
+        // TODO is this the preferred way to remove a property from an object?
+        this.ship = undefined;
         super.playerDestroyed();
-        // TODO implement me
+        if (--this.player.lives) {
+            this.scheduler.schedule(this.createShip.bind(this), TRANSITION_DELAY);
+        }
     }
 }
